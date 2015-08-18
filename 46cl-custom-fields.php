e512f77c71b693e5b3acfae9d1e222ce621eded2
Johann Pardanaud <pardanaud.j@gmail.com>
2015-08-18 16:10:45 +0200

    Release v0.3.0

--- 46cl-custom-fields.php
+++ 46cl-custom-fields.php
@@ -3,7 +3,7 @@
 Plugin Name: 46cl - Custom Fields
 Plugin URI:  https://github.com/46cl/wp-custom-fields
 Description: Custom fields created by 46cl for Wordpress
-Version:     0.2.2
+Version:     0.3.0
 Author:      46cl
 Author URI:  http://46cl.fr
 */
